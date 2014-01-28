3a0323e1aac71a005da2d0898374a02a0aa11cad
Edward Anderson <ejanderson4@gmail.com>
2014-01-28 16:44:50 -0600

    now using nokogiri

--- email_finder.rb
+++ email_finder.rb
@@ -3,36 +3,54 @@ require "open-uri"
 require 'rubygems' 
 require 'bundler/setup'  # require your gems as usual require 'nokogiri'
 require 'httparty'
+require 'nokogiri'
+require 'pry'
 
 class EmailFinder
-  DEEPNESS=3
+  DEEPNESS=4
   EMAIL_REGEX =/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
-  HREF_REXEX = /href="([^"]*)"/
+  #HREF_REXEX = /href="([^"]*)"/
 
   def self.download_email_addresses_from_url url
     uri = URI url
     results = self.parse_page_for_emails uri
+   
     results.each do |key,email|
       puts "#{email[:email]} #{email[:uri]}"
     end
+
   end
 
   private
 
     def self.parse_page_for_emails uri, emails={}, hrefs=[], deepness=0
-    #puts uri.to_s
       html =HTTParty.get uri.to_s
-    #puts html
+
+      doc = Nokogiri::HTML(html)
+      
       html.scan(EmailFinder::EMAIL_REGEX).each do |email_match|
         emails[email_match] = {:email => email_match, :uri => uri.to_s } 
       end
 
-      html.scan(EmailFinder::HREF_REXEX).each do |href_match_array|
-        href_match= href_match_array[0]
+      doc.search('a').each do |element|
+        href_match= element.attributes['href'].value
   
-        href_match ="#{uri.scheme}://#{uri.host}#{href_match}" if  href_match[0,1]=='/'
-        new_uri = URI href_match
-        if !hrefs.include?(href_match) && uri.host  == new_uri.host && deepness <= EmailFinder::DEEPNESS
+        if href_match[0,1]=='/'
+          new_path ="#{uri.scheme}://#{uri.host}/#{href_match}" 
+        elsif !href_match.match('http') 
+
+          folder_array = uri.path.split('/')
+          folder_array.pop
+          folder=folder_array.join('/')
+
+          new_path ="#{uri.scheme}://#{uri.host}#{folder}/#{href_match}"
+        else
+          new_path=href_match
+        end
+
+
+        new_uri = URI new_path
+        if !hrefs.include?(href_match) && uri.host  == new_uri.host && deepness < EmailFinder::DEEPNESS
           begin
             hrefs << href_match
             self.parse_page_for_emails new_uri, emails, hrefs, deepness+1
